88329c3ab76224146bd79e54ecd51708e4bc2f60
Lukáš Janeček <xjacka@users.noreply.github.com>
2025-04-23 10:56:00 +0200

    feat(examples): update approval agent (#37)
    Signed-off-by: Lukáš Janeček <xjacka@gmail.com>

--- examples/python/basic/await-external-approval/agent.py
+++ examples/python/basic/await-external-approval/agent.py
@@ -1,7 +1,10 @@
+import asyncio
 from collections.abc import AsyncGenerator
+import random
+import string
 
 from acp_sdk import Message
-from acp_sdk.models import AwaitRequest, MessagePart
+from acp_sdk.models import MessageAwaitRequest, MessagePart
 from acp_sdk.server import Context, Server
 
 server = Server()
@@ -10,12 +13,18 @@ server = Server()
 @server.agent()
 async def approval_agent(inputs: list[Message], context: Context) -> AsyncGenerator:
     """Request approval and respond to user's confirmation."""
-    yield MessagePart(content="Hello! I need an approval.")
 
     # Pause execution and wait for external confirmation
-    yield AwaitRequest()
-
-    yield MessagePart(content="Thank you for approving!")
+    response = yield MessageAwaitRequest(message=Message(parts=[MessagePart(content="I can generate password for you. Do you want me to do that?")]))
+    if str(response.message) == "yes":
+        # User approved, continue execution
+        yield MessagePart(content="Generating password...")
+        # Simulate password generation
+        await asyncio.sleep(1)
+        yield Message(parts=[MessagePart(content=f"Your password is: {''.join(random.choices(string.ascii_letters, k=10))}")])
+    else:
+        # User declined, stop execution
+        yield MessagePart(content="Password generation declined.")
 
 
 server.run()
--- examples/python/basic/await-external-approval/client.py
+++ examples/python/basic/await-external-approval/client.py
@@ -1,17 +1,22 @@
 import asyncio
+from functools import reduce
 
 from acp_sdk.client import Client
-from acp_sdk.models import AwaitResume, Message, MessagePart
+from acp_sdk.models import MessageAwaitResume, Message, MessagePart
 
 
 async def handle_resume(client, run_id):
-    async for event in client.run_resume_stream(run_id=run_id, await_resume=AwaitResume()):
+    async for event in client.run_resume_stream(run_id=run_id, await_resume=MessageAwaitResume(message=Message(parts=[MessagePart(content="yes")]))):
         print(event)
+        
+        if event.type == "run.completed":
+            print()
+            print(str(event.run.outputs[-1]))
 
 
 async def client():
     async with Client(base_url="http://localhost:8000") as client:
-        initial_message = Message(parts=[MessagePart(content="Hi there!")])
+        initial_message = Message(parts=[MessagePart(content="Can you generate a password for me?")])
 
         async for event in client.run_stream(agent="approval_agent", inputs=[initial_message]):
             print(event)
