0001e0d15483283d3987ae488f38dd1f0c45943a
Tomas Pilar <tomas.pilar@apoco.com>
2025-04-25 11:01:25 +0200

    chore: add agent discovery tests
    Signed-off-by: Tomas Pilar <tomas.pilar@apoco.com>

---
+++ python/tests/e2e/test_suites/test_discovery.py
@@ -0,0 +1,18 @@
+import pytest
+from acp_sdk.client import Client
+from acp_sdk.models import Agent
+from acp_sdk.server import Server
+
+
+@pytest.mark.asyncio
+async def test_agents_list(server: Server, client: Client) -> None:
+    async for agent in client.agents():
+        assert isinstance(agent, Agent)
+
+
+@pytest.mark.asyncio
+async def test_agents_details(server: Server, client: Client) -> None:
+    agent_name = "echo"
+    agent = await client.agent(name=agent_name)
+    assert isinstance(agent, Agent)
+    assert agent.name == agent_name
